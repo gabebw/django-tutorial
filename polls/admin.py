d3ee16697408cbd578c71c24996590a28d554039
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-03 21:03:20 -0700

    Change field order in Question admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # pub_date will be displayed before question_text
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
