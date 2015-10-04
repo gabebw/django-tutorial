3c9e9922067f6aa9e2cc2379a8890af559ccd2c9
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-03 21:04:37 -0700

    Split fields into fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,11 @@ from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     # pub_date will be displayed before question_text
-    fields = ['pub_date', 'question_text']
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
