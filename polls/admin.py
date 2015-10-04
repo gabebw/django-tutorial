f36d8d6e1ec1d251b87cc87f7757f0f8b9c148d9
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-03 21:05:33 -0700

    Add Choices right on the Question admin page

--- polls/admin.py
+++ polls/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
-    # pub_date will be displayed before question_text
-    # fields = ['pub_date', 'question_text']
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
