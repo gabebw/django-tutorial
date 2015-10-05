627e1174b35cd37274646d662689eb0ec615d2c7
Gabe Berke-Williams <gabebw@gabebw.com>
2015-10-04 18:16:56 -0700

    Add some more views

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # /polls/
+    url(r'^$', views.index, name='index'),
+    # /polls/:question_id
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # /polls/:question_id/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # /polls/:question_id/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
